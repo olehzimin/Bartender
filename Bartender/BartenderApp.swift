eabecd3ae6b1badcc43c4aa1f676c6af555e9434
Oleh Zimin <ziminoleh@gmail.com>
2025-04-11 23:38:18 +0200

    Initial Commit

---
+++ Bartender/BartenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BartenderApp.swift
+//  Bartender
+//
+//  Created by Oleh Zimin on 11.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct BartenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
